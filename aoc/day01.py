9248cea3999607fac1764edbb4862d5d8019402f
Matt Chun-Lum <matt@questionable.engineering>
2023-12-01 00:43:41 -0800

    perf: use isdigit

--- aoc/day01.py
+++ aoc/day01.py
@@ -11,49 +11,33 @@ class Solver(aoc.util.Solver):
         self.lines = self.input.splitlines()
 
     def match_simple_digit(self, ch: str):
-        if ch == '0':
-            return 0
-        elif ch == '1':
-            return 1
-        elif ch == '2':
-            return 2
-        elif ch == '3':
-            return 3
-        elif ch == '4':
-            return 4
-        elif ch == '5':
-            return 5
-        elif ch == '6':
-            return 6
-        elif ch == '7':
-            return 7
-        elif ch == '8':
-            return 8
-        elif ch == '9':
-            return 9
+        if ch.isdigit():
+            return int(ch)
         else:
             return None
 
     def match_slice(self, slice: str):
-        if slice.startswith("0") or slice.startswith("zero"):
+        if slice[0].isdigit():
+            return int(slice[0])
+        if slice.startswith("zero"):
             return 0
-        elif slice.startswith("1") or slice.startswith("one"):
+        elif slice.startswith("one"):
             return 1
-        elif slice.startswith("2") or slice.startswith("two"):
+        elif slice.startswith("two"):
             return 2
-        elif slice.startswith("3") or slice.startswith("three"):
+        elif slice.startswith("three"):
             return 3
-        elif slice.startswith("4") or slice.startswith("four"):
+        elif slice.startswith("four"):
             return 4
-        elif slice.startswith("5") or slice.startswith("five"):
+        elif slice.startswith("five"):
             return 5
-        elif slice.startswith("6") or slice.startswith("six"):
+        elif slice.startswith("six"):
             return 6
-        elif slice.startswith("7") or slice.startswith("seven"):
+        elif slice.startswith("seven"):
             return 7
-        elif slice.startswith("8") or slice.startswith("eight"):
+        elif slice.startswith("eight"):
             return 8
-        elif slice.startswith("9") or slice.startswith("nine"):
+        elif slice.startswith("nine"):
             return 9
         else:
             return None
