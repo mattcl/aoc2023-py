d01c2662c001fdeba68de8275f0a90385a114846
Matt Chun-Lum <matt@questionable.engineering>
2023-12-01 10:32:31 -0800

    perf: calculate part 2 first, set lookup for first char

--- aoc/day01.py
+++ aoc/day01.py
@@ -2,13 +2,50 @@
 import aoc.util
 
 
+STARTING = set(['o', 't', 'f', 's', 'e', 'n'])
+
+
 # all solutions should subclass the `Solver` exposed by `aoc.util`
 # this class MUST be called Solver for the CLI discovery to work
 class Solver(aoc.util.Solver):
     def __init__(self, input: str):
         # sets self.input to the provided input
         super(Solver, self).__init__(input)
-        self.lines = input.splitlines()
+        lines = input.splitlines()
+
+        self.p1_sum = 0
+        self.p2_sum = 0
+
+        for line in lines:
+            line_len = len(line)
+
+            start = 0
+            end = 0
+
+            for s_idx in range(0, line_len):
+                v = self.match_line(s_idx, line)
+                if v is not None:
+                    self.p2_sum += v * 10
+                    start = s_idx
+                    break
+
+            for s_idx in range(0, line_len):
+                v = self.match_line(line_len - 1 - s_idx, line)
+                if v is not None:
+                    self.p2_sum += v
+                    end = s_idx
+                    break
+
+            for s_idx in range(start, line_len):
+                if line[s_idx].isdigit():
+                    self.p1_sum += int(line[s_idx]) * 10
+                    break
+
+            for s_idx in range(end, line_len):
+                adjust = line_len - 1 - s_idx
+                if line[adjust].isdigit():
+                    self.p1_sum += int(line[adjust])
+                    break
 
     def match_simple_digit(self, ch: str):
         if ch.isdigit():
@@ -20,71 +57,33 @@ class Solver(aoc.util.Solver):
         first = line[idx]
         if first.isdigit():
             return int(first)
-        elif first == "t":
-            if line.startswith("two", idx):
-                return 2
-            elif line.startswith("three", idx):
-                return 3
-            else:
-                return None
-        elif first == "s":
-            if line.startswith("six", idx):
-                return 6
-            elif line.startswith("seven", idx):
-                return 7
-            else:
-                return None
-        elif first == "f":
-            if line.startswith("four", idx):
-                return 4
-            elif line.startswith("five", idx):
-                return 5
-            else:
-                return None
-        elif line.startswith("one", idx):
-            return 1
-        elif line.startswith("eight", idx):
-            return 8
-        elif line.startswith("nine", idx):
-            return 9
-        else:
-            return None
+        elif first in STARTING:
+            if first == "t":
+                if line.startswith("two", idx):
+                    return 2
+                elif line.startswith("three", idx):
+                    return 3
+            elif first == "s":
+                if line.startswith("six", idx):
+                    return 6
+                elif line.startswith("seven", idx):
+                    return 7
+            elif first == "f":
+                if line.startswith("four", idx):
+                    return 4
+                elif line.startswith("five", idx):
+                    return 5
+            elif line.startswith("one", idx):
+                return 1
+            elif line.startswith("eight", idx):
+                return 8
+            elif line.startswith("nine", idx):
+                return 9
+
+        return None
 
     def part_one(self) -> int:
-        sum = 0
-        for line in self.lines:
-            for ch in line:
-                v = self.match_simple_digit(ch)
-                if v is not None:
-                    sum += v * 10
-                    break
-
-            l = len(line)
-
-            for i in range(0, l):
-                ch = line[l - 1 - i]
-                v = self.match_simple_digit(ch)
-                if v is not None:
-                    sum += v
-                    break
-
-        return sum
+        return self.p1_sum
 
     def part_two(self) -> int:
-        sum = 0
-        for line in self.lines:
-            line_len = len(line)
-
-            for s_idx in range(0, line_len):
-                v = self.match_line(s_idx, line)
-                if v is not None:
-                    sum += v * 10
-                    break
-
-            for s_idx in range(0, line_len):
-                v = self.match_line(line_len - 1 - s_idx, line)
-                if v is not None:
-                    sum += v
-                    break
-
-        return sum
+        return self.p2_sum
