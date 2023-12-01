0efa6b92367d4786fae750cfaa2559f3e65c0857
Matt Chun-Lum <matt@questionable.engineering>
2023-12-01 01:35:07 -0800

    perf: avoid making new slices

--- aoc/day01.py
+++ aoc/day01.py
@@ -8,7 +8,7 @@ class Solver(aoc.util.Solver):
     def __init__(self, input: str):
         # sets self.input to the provided input
         super(Solver, self).__init__(input)
-        self.lines = self.input.splitlines()
+        self.lines = input.splitlines()
 
     def match_simple_digit(self, ch: str):
         if ch.isdigit():
@@ -16,37 +16,36 @@ class Solver(aoc.util.Solver):
         else:
             return None
 
-    def match_slice(self, slice: str):
-        if slice[0].isdigit():
-            return int(slice[0])
-        elif slice[0] == "t":
-            if slice.startswith("two"):
+    def match_line(self, idx: int, line: str):
+        first = line[idx]
+        if first.isdigit():
+            return int(first)
+        elif first == "t":
+            if line.startswith("two", idx):
                 return 2
-            elif slice.startswith("three"):
+            elif line.startswith("three", idx):
                 return 3
             else:
                 return None
-        elif slice[0] == "s":
-            if slice.startswith("six"):
+        elif first == "s":
+            if line.startswith("six", idx):
                 return 6
-            elif slice.startswith("seven"):
+            elif line.startswith("seven", idx):
                 return 7
             else:
                 return None
-        elif slice[0] == "f":
-            if slice.startswith("four"):
+        elif first == "f":
+            if line.startswith("four", idx):
                 return 4
-            elif slice.startswith("five"):
+            elif line.startswith("five", idx):
                 return 5
             else:
                 return None
-        elif slice.startswith("zero"):
-            return 0
-        elif slice.startswith("one"):
+        elif line.startswith("one", idx):
             return 1
-        elif slice.startswith("eight"):
+        elif line.startswith("eight", idx):
             return 8
-        elif slice.startswith("nine"):
+        elif line.startswith("nine", idx):
             return 9
         else:
             return None
@@ -77,15 +76,13 @@ class Solver(aoc.util.Solver):
             line_len = len(line)
 
             for s_idx in range(0, line_len):
-                slice = line[s_idx:]
-                v = self.match_slice(slice)
+                v = self.match_line(s_idx, line)
                 if v is not None:
                     sum += v * 10
                     break
 
             for s_idx in range(0, line_len):
-                slice = line[(line_len - 1 - s_idx):]
-                v = self.match_slice(slice)
+                v = self.match_line(line_len - 1 - s_idx, line)
                 if v is not None:
                     sum += v
                     break
