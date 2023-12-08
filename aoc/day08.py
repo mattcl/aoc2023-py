a94c2e8112cb16ea59a3e12ac2e7be2de443f359
Matt Chun-Lum <matt@questionable.engineering>
2023-12-07 23:21:59 -0800

    feat: day 8

---
+++ aoc/day08.py
@@ -0,0 +1,71 @@
+"""08: PROBLEM NAME"""
+from functools import reduce
+import itertools
+import math
+
+import aoc.util
+
+
+class Node:
+    def __init__(self, key, left, right):
+        self.left = left
+        self.right = right
+        self.ends_with_a = key.endswith("A")
+        self.ends_with_z = key.endswith("Z")
+
+
+class Solver(aoc.util.Solver):
+    def __init__(self, input: str):
+        super(Solver, self).__init__(input)
+        parts = input.split("\n\n")
+        self.instructions = parts[0]
+        self.mapping = {}
+        for line in parts[1].splitlines():
+            components = line.split(" = ")
+            key = components[0]
+            sides = components[1].replace("(", "").replace(")", "").split(", ")
+            self.mapping[key] = Node(key, sides[0], sides[1])
+
+    def part_one(self) -> int:
+        count = 0
+        cur = "AAA"
+        cur_node = self.mapping[cur]
+
+        for i in itertools.cycle(self.instructions):
+            if cur == "ZZZ":
+                return count
+
+            if i == "L":
+                cur = cur_node.left
+            else:
+                cur = cur_node.right
+
+            cur_node = self.mapping[cur]
+            count += 1
+
+        # should be impossible to get here
+        return -1
+
+    def part_two(self) -> int:
+        starting = (self.get_first_z(x) for x, y in self.mapping.items() if y.ends_with_a)
+        return math.lcm(reduce(lambda x, y: x * y // math.gcd(x, y), starting, 1))
+
+    def get_first_z(self, start):
+        count = 0
+        cur = start
+        cur_node = self.mapping[cur]
+
+        for i in itertools.cycle(self.instructions):
+            if cur_node.ends_with_z:
+                return count
+
+            if i == "L":
+                cur = cur_node.left
+            else:
+                cur = cur_node.right
+
+            cur_node = self.mapping[cur]
+            count += 1
+
+        # should be impossible to get here
+        return -1
---
+++ tests/test_day08.py
@@ -0,0 +1,88 @@
+import pytest
+
+from aoc.day08 import Solver
+from aoc.util import Solution
+
+
+#############################
+# ======= solutons =========#
+#############################
+EXAMPLE_PART_ONE = 2
+EXAMPLE_PART_TWO = 6
+PART_ONE = 12361
+PART_TWO = 18215611419223
+
+
+#############################
+# ========= setup ==========#
+#############################
+@pytest.fixture
+def example_input() -> str:
+    with open("inputs/day08_example.txt", "r") as f:
+        return f.read()
+
+
+@pytest.fixture
+def example_input2() -> str:
+    with open("inputs/day08_example2.txt", "r") as f:
+        return f.read()
+
+
+@pytest.fixture
+def real_input() -> str:
+    with open("inputs/day08.txt", "r") as f:
+        return f.read()
+
+
+@pytest.fixture
+def example_solver(example_input: str) -> Solver:
+    return Solver(example_input)
+
+
+@pytest.fixture
+def example_solver2(example_input2: str) -> Solver:
+    return Solver(example_input2)
+
+
+@pytest.fixture
+def real_solver(real_input: str) -> Solver:
+    return Solver(real_input)
+
+
+#############################
+# === tests for part one ===#
+#############################
+@pytest.mark.example
+def test_example_part_one(example_solver: Solver):
+    assert example_solver.part_one() == EXAMPLE_PART_ONE
+
+
+@pytest.mark.real
+def test_real_part_one(real_solver: Solver):
+    assert real_solver.part_one() == PART_ONE
+
+
+#
+#############################
+# === tests for part two ===#
+#############################
+@pytest.mark.example
+def test_example_part_two(example_solver2: Solver):
+    assert example_solver2.part_two() == EXAMPLE_PART_TWO
+
+
+@pytest.mark.real
+def test_real_part_two(real_solver: Solver):
+    assert real_solver.part_two() == PART_TWO
+
+
+#############################
+# ======= benchmarks =======#
+#############################
+@pytest.mark.bench
+def test_day08(benchmark, real_input: str):
+    expected = Solution(part_one=PART_ONE, part_two=PART_TWO)
+    result = benchmark(Solver.solve, real_input)
+
+    # let's just leverage the diffs pytest will provide for better output
+    assert result.__dict__ == expected.__dict__
