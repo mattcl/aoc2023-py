68266a8e09ebca2f51f45a3cf3b019ceb614cd8c
Matt Chun-Lum <matt@questionable.engineering>
2023-12-11 20:16:20 -0800

    perf(day10): shoelace

--- aoc/day10.py
+++ aoc/day10.py
@@ -9,28 +9,6 @@ class Directions(Enum):
     SOUTH = 2
     WEST = 3
 
-    def right(self):
-        match self:
-            case Directions.NORTH:
-                return Directions.EAST
-            case Directions.EAST:
-                return Directions.SOUTH
-            case Directions.SOUTH:
-                return Directions.WEST
-            case Directions.WEST:
-                return Directions.NORTH
-
-    def left(self):
-        match self:
-            case Directions.NORTH:
-                return Directions.WEST
-            case Directions.WEST:
-                return Directions.SOUTH
-            case Directions.SOUTH:
-                return Directions.EAST
-            case Directions.EAST:
-                return Directions.NORTH
-
 
 class Location:
     def __init__(self, row, col):
@@ -42,6 +20,24 @@ class Location:
 
     def __eq__(self, other):
         return isinstance(other, Location) and self.row == other.row and self.col == other.col
+
+    # this is a much dumber version than the one in my rust solution
+    def tile_between(self, other, start):
+        if self.row == other.row:
+            return HORIZONAL
+        elif self.col == other.col:
+            return VERTICAL
+        elif self.col < other.col and self.row < other.row:
+            return SW90
+        elif self.col > other.col and self.row > other.row:
+            return NE90
+        elif self.col < other.col and self.row > other.row:
+            if self.row == start.row:
+                return NW90
+            elif self.col == start.col:
+                return SE90
+
+        assert False, "should have been unreachable"
 
 
 NEIGHBORS = [
@@ -60,7 +56,6 @@ SW90 = 4
 SE90 = 5
 GROUND = 6
 START = 7
-PROCESSED = 8
 
 
 TRANSLATE = {
@@ -75,71 +70,47 @@ TRANSLATE = {
 }
 
 
-class State:
-    def __init__(self, location, facing, tile):
-        self.location = location
-        self.facing = facing
-        self.tile = tile
-
-    def right_locs(self, maze, width, height):
-        out = []
-        if self.tile == NW90 and self.facing == Directions.NORTH:
-            loc = self.location.add(NEIGHBORS[Directions.SOUTH.value])
-            if maze[loc.row][loc.col] != PROCESSED:
-                out.append(loc)
-        elif self.tile == SE90 and self.facing == Directions.SOUTH:
-            loc = self.location.add(NEIGHBORS[Directions.NORTH.value])
-            if maze[loc.row][loc.col] != PROCESSED:
-                out.append(loc)
-        elif self.tile == NE90 and self.facing == Directions.EAST:
-            loc = self.location.add(NEIGHBORS[Directions.WEST.value])
-            if maze[loc.row][loc.col] != PROCESSED:
-                out.append(loc)
-        elif self.tile == SW90 and self.facing == Directions.WEST:
-            loc = self.location.add(NEIGHBORS[Directions.EAST.value])
-            if maze[loc.row][loc.col] != PROCESSED:
-                out.append(loc)
-
-        loc = self.location.add(NEIGHBORS[self.facing.right().value])
-        if maze[loc.row][loc.col] != PROCESSED:
-            out.append(loc)
-
-        return out
-
-    def left_locs(self, maze, width, height):
-        out = []
-        if self.tile == NE90 and self.facing == Directions.NORTH:
-            loc = self.location.add(NEIGHBORS[Directions.SOUTH.value])
-            if maze[loc.row][loc.col] != PROCESSED:
-                out.append(loc)
-        elif self.tile == SW90 and self.facing == Directions.SOUTH:
-            loc = self.location.add(NEIGHBORS[Directions.NORTH.value])
-            if maze[loc.row][loc.col] != PROCESSED:
-                out.append(loc)
-        elif self.tile == SE90 and self.facing == Directions.EAST:
-            loc = self.location.add(NEIGHBORS[Directions.WEST.value])
-            if maze[loc.row][loc.col] != PROCESSED:
-                out.append(loc)
-        elif self.tile == NW90 and self.facing == Directions.WEST:
-            loc = self.location.add(NEIGHBORS[Directions.EAST.value])
-            if maze[loc.row][loc.col] != PROCESSED:
-                out.append(loc)
-
-        loc = self.location.add(NEIGHBORS[self.facing.left().value])
-        if maze[loc.row][loc.col] != PROCESSED:
-            out.append(loc)
-
-        return out
-
-
-
 class Actor:
     def __init__(self, location, facing, tile):
         self.location = location
         self.facing = facing
+        self.tile = tile
         self.right_turns = 0
         self.left_turns = 0
-        self.history = [State(location, facing, tile)]
+
+    def shoelace_right(self):
+        if self.tile == VERTICAL:
+            if self.facing == Directions.SOUTH:
+                return self.location.col - 1
+            else:
+                return -self.location.col
+        elif self.tile == NW90 and self.facing == Directions.NORTH:
+            return -self.location.col
+        elif self.tile == SW90 and self.facing == Directions.WEST:
+            return -self.location.col
+        elif self.tile == NE90 and self.facing == Directions.EAST:
+            return self.location.col - 1
+        elif self.tile == SE90 and self.facing == Directions.SOUTH:
+            return self.location.col - 1
+        else:
+            return 0
+
+    def shoelace_left(self):
+        if self.tile == VERTICAL:
+            if self.facing == Directions.NORTH:
+                return self.location.col - 1
+            else:
+                return -self.location.col
+        elif self.tile == NW90 and self.facing == Directions.WEST:
+            return -self.location.col
+        elif self.tile == SW90 and self.facing == Directions.SOUTH:
+            return -self.location.col
+        elif self.tile == NE90 and self.facing == Directions.NORTH:
+            return self.location.col - 1
+        elif self.tile == SE90 and self.facing == Directions.EAST:
+            return self.location.col - 1
+        else:
+            return 0
 
     def advance(self, grid):
         next_location = self.location.add(NEIGHBORS[self.facing.value])
@@ -176,8 +147,7 @@ class Actor:
                 self.right_turns += 1
 
         self.location = next_location
-
-        self.history.append(State(self.location, self.facing, tile))
+        self.tile = tile
 
 
 class Solver(aoc.util.Solver):
@@ -195,86 +165,67 @@ class Solver(aoc.util.Solver):
         self.width = len(self.grid[0])
 
         steps = 1
-
         actor = self.determine_starting_actor()
-        self.grid[self.start.row][self.start.col] = PROCESSED
-        self.grid[actor.location.row][actor.location.col] = PROCESSED
+        shoelace_right = actor.shoelace_right()
+        shoelace_left = actor.shoelace_left()
 
         while not actor.location == self.start:
             steps += 1
             actor.advance(self.grid)
-            self.grid[actor.location.row][actor.location.col] = PROCESSED
+            shoelace_right += actor.shoelace_right()
+            shoelace_left += actor.shoelace_left()
 
-        self.num_inside = 0
         # if we have more right turns than left turns, the tiles in the loop are
         # to our right, otherwise, they're to our left
         if actor.right_turns > actor.left_turns:
-            for state in actor.history:
-                locs = state.right_locs(self.grid, self.width, self.height)
-
-                if len(locs) > 0:
-                    self.flood(locs)
+            self.num_inside = shoelace_right
         else:
-            for state in actor.history:
-                locs = state.left_locs(self.grid, self.width, self.height)
-
-                if len(locs) > 0:
-                    self.flood(locs)
+            self.num_inside = shoelace_left
 
         self.steps = steps // 2
 
     def determine_starting_actor(self):
         # determine a position off of the starting location to move from
         dirs = [Directions.NORTH, Directions.EAST, Directions.SOUTH, Directions.WEST]
+        actors = []
         for dir in dirs:
             loc = self.start.add(NEIGHBORS[dir.value])
             if loc.row >= 0 and loc.col >= 0 and loc.row < self.height and loc.col < self.width:
                 tile = self.grid[loc.row][loc.col]
                 if tile == VERTICAL and (dir == Directions.NORTH or dir == Directions.SOUTH):
-                    return Actor(loc, dir, tile)
+                    actors.append(Actor(loc, dir, tile))
 
                 if tile == HORIZONAL and (dir == Directions.WEST or dir == Directions.EAST):
-                    return Actor(loc, dir, tile)
+                    actors.append(Actor(loc, dir, tile))
 
                 if tile == SW90 and dir == Directions.NORTH:
-                    return Actor(loc, Directions.WEST, tile)
+                    actors.append(Actor(loc, Directions.WEST, tile))
 
                 if tile == SE90 and dir == Directions.NORTH:
-                    return Actor(loc, Directions.EAST, tile)
+                    actors.append(Actor(loc, Directions.EAST, tile))
 
                 if tile == NW90 and dir == Directions.SOUTH:
-                    return Actor(loc, Directions.WEST, tile)
+                    actors.append(Actor(loc, Directions.WEST, tile))
 
                 if tile == NE90 and dir == Directions.SOUTH:
-                    return Actor(loc, Directions.EAST, tile)
+                    actors.append(Actor(loc, Directions.EAST, tile))
 
                 if tile == SW90 and dir == Directions.EAST:
-                    return Actor(loc, Directions.SOUTH, tile)
+                    actors.append(Actor(loc, Directions.SOUTH, tile))
 
                 if tile == NW90 and dir == Directions.EAST:
-                    return Actor(loc, Directions.NORTH, tile)
+                    actors.append(Actor(loc, Directions.NORTH, tile))
 
                 if tile == SE90 and dir == Directions.WEST:
-                    return Actor(loc, Directions.SOUTH, tile)
+                    actors.append(Actor(loc, Directions.SOUTH, tile))
 
                 if tile == NE90 and dir == Directions.WEST:
-                    return Actor(loc, Directions.NORTH, tile)
+                    actors.append(Actor(loc, Directions.NORTH, tile))
 
-    def flood(self, cur):
-        next = []
-        for cur_loc in cur:
-            if self.grid[cur_loc.row][cur_loc.col] == PROCESSED:
-                continue
-            self.grid[cur_loc.row][cur_loc.col] = PROCESSED
-            self.num_inside += 1
+        start_tile = actors[1].location.tile_between(actors[0].location, self.start)
+        self.grid[self.start.row][self.start.col] = start_tile
 
-            for neighbor in NEIGHBORS:
-                loc = cur_loc.add(neighbor)
-                if self.grid[loc.row][loc.col] != PROCESSED:
-                    next.append(loc)
-
-        if len(next) > 0:
-            self.flood(next)
+        return actors[0]
 
     def part_one(self) -> int:
         return self.steps
