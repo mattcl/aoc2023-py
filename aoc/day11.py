90f20ef5856ca98466a2a877d01124dec6fa5848
Matt Chun-Lum <matt@questionable.engineering>
2023-12-15 11:47:46 -0800

    perf(day11): linear solution

--- aoc/day11.py
+++ aoc/day11.py
@@ -4,67 +4,42 @@ from itertools import combinations, pairwise
 import aoc.util
 
 
-class Location:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+def axis_sum(pos_counts, expansion):
+    total = 0
+    distance = 0
+    offset = 0
+    global_idx = 0
+    for idx, count in enumerate(pos_counts):
+        if count == 0:
+            offset += expansion
+        else:
+            expansion_offset = idx + offset
+            for _ in range(0, count):
+                distance += global_idx * expansion_offset - total
+                total += expansion_offset
+                global_idx += 1
 
-    def manhattan_distance(self, other):
-        return abs(self.x - other.x) + abs(self.y - other.y)
-
-
-class Galaxy:
-    def __init__(self, location):
-        self.original = location
-        self.one = copy(location)
-        self.one_million = copy(location)
+    return distance
 
 
 class Solver(aoc.util.Solver):
     def __init__(self, input: str):
         lines = input.splitlines()
         width = len(lines[0])
-        galaxies = []
-        num_empty_rows = 0
-        empty_cols_raw = set(range(0, width))
+        row_counts = []
+        col_counts = [0] * width
 
-        for row, line in enumerate(input.splitlines()):
-            seen_row = False
+        for line in lines:
+            row_count = 0
             for col, ch in enumerate(line):
                 if ch == '#':
-                    seen_row = True
-                    galaxy = Galaxy(Location(col, row))
-                    galaxy.one.y += num_empty_rows
-                    galaxy.one_million.y += num_empty_rows * 999999
-                    galaxies.append(galaxy)
-                    if col in empty_cols_raw:
-                        empty_cols_raw.remove(col)
+                    col_counts[col] += 1
+                    row_count += 1
 
-            if not seen_row:
-                num_empty_rows += 1
+            row_counts.append(row_count)
 
-        empty_cols = list(sorted(empty_cols_raw))
-        empty_cols.append(width)
-
-        cols_iter = list(reversed(list(enumerate(empty_cols))))
-
-        counts = [0] * width
-
-        for (idx1, col1), (idx2, col2) in pairwise(enumerate(empty_cols)):
-            for j in range(col1, col2):
-                counts[j] = idx1 + 1
-
-        for g in galaxies:
-            multiple = counts[g.original.x]
-            g.one.x += multiple
-            g.one_million.x += 999999 * multiple
-
-        self.p1 = 0
-        self.p2 = 0
-
-        for a, b in combinations(galaxies, 2):
-            self.p1 += a.one.manhattan_distance(b.one)
-            self.p2 += a.one_million.manhattan_distance(b.one_million)
+        self.p1 = axis_sum(row_counts, 1) + axis_sum(col_counts, 1)
+        self.p2 = axis_sum(row_counts, 999999) + axis_sum(col_counts, 999999)
 
     def part_one(self) -> int:
         return self.p1
