c07eca8e02dce4566c8c6804e8f00e58455a08da
Matt Chun-Lum <matt@questionable.engineering>
2024-01-07 01:21:14 -0800

    perf(day22): topology in single pass

--- aoc/day22.py
+++ aoc/day22.py
@@ -6,19 +6,6 @@ from multiprocessing import Pool
 import aoc.util
 
 
-def rect_intersection(this, other) -> bool:
-    begin_x = max(this[0], other[0])
-    end_x = min(this[2], other[2])
-
-    if begin_x > end_x:
-        return False
-
-    begin_y = max(this[1], other[1])
-    end_y = min(this[3], other[3])
-
-    return begin_y <= end_y
-
-
 def set_z(cube, z):
     delta = z - cube[0][2]
     cube[0][2] += delta
@@ -61,51 +48,68 @@ class Solver(aoc.util.Solver):
             start_components, end_components = line.split('~')
             start = list(map(int, start_components.split(',')))
             end = list(map(int, end_components.split(',')))
-            # (x0, y0, x1, y1)
-            xy_rect = (
-                min(start[0], end[0]),
-                min(start[1], end[1]),
-                max(start[0], end[0]),
-                max(start[1], end[1]),
-            )
-            # (start, end, xy_rect)
-            self.cubes.append([start, end, xy_rect])
+            # (start, end)
+            self.cubes.append([start, end])
 
         self.cubes.sort(key=lambda x: x[0][2])
 
     def part_one(self) -> int:
-        # [z_height, index]
-        stable_bricks = []
+        max_x = 0
+        max_y = 0
+
+        for cube in self.cubes:
+            if cube[0][0] > max_x:
+                max_x = cube[0][0]
+
+            if cube[1][0] > max_x:
+                max_x = cube[1][0]
+
+            if cube[0][1] > max_y:
+                max_y = cube[0][1]
+
+            if cube[1][1] > max_y:
+                max_y = cube[1][1]
+
+        topology = [[-1 for _ in range(max_x + 2)] for _ in range(max_y + 2)]
+
         self.above = defaultdict(list)
         self.below = defaultdict(list)
+
+        self.required = set()
+
         for i in range(len(self.cubes)):
             brick = self.cubes[i]
 
-            if brick[0][2] > 1:
-                stable_bricks.sort()
-                max_height = -1
-                for (entry_z, entry_idx) in reversed(stable_bricks):
-                    next_z = entry_z + 1
-                    cur_b = self.cubes[entry_idx][2]
+            highest = 0
+            highest_idxs = []
 
-                    if next_z >= max_height:
-                        if rect_intersection(cur_b, brick[2]):
-                            if max_height == -1:
-                                max_height = next_z
+            sx = min(brick[0][0], brick[1][0])
+            ex = max(brick[0][0], brick[1][0]) + 1
+            sy = min(brick[0][1], brick[1][1])
+            ey = max(brick[0][1], brick[1][1]) + 1
 
-                            self.above[entry_idx].append(i)
-                            self.below[i].append(entry_idx)
-                    else:
-                        break
+            for x in range(sx, ex):
+                for y in range(sy, ey):
+                    idx = topology[y][x]
 
-                if max_height == -1:
-                    set_z(self.cubes[i], 1)
-                else:
-                    set_z(self.cubes[i], max_height)
+                    if idx != -1 and self.cubes[idx][1][2] >= highest:
+                        if self.cubes[idx][1][2] > highest:
+                            highest_idxs.clear()
+                            highest = self.cubes[idx][1][2]
 
-            stable_bricks.append((self.cubes[i][1][2], i))
+                        if len(highest_idxs) == 0 or highest_idxs[-1] != idx:
+                            highest_idxs.append(idx)
 
-        self.required = set((x[0] for x in self.below.values() if len(x) == 1))
+                    topology[y][x] = i
+
+            if len(highest_idxs) == 1:
+                self.required.add(highest_idxs[0])
+
+            for idx in highest_idxs:
+                self.above[idx].append(i)
+                self.below[i].append(idx)
+
+            set_z(self.cubes[i], highest + 1)
 
         return len(self.cubes) - len(self.required)
 
