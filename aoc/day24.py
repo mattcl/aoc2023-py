fix(day24): switch numpy for sympy because of precision

aoc/day24.py
@@ -1,9 +1,9 @@
 """24: PROBLEM NAME"""
 from itertools import combinations
 
-# Sigh. There aren't many python linear algebra libraries that _aren't_ numpy.
 # I'd prefer not to have this dependency at all
-import numpy as np
+# import numpy as np  # -> numpy precision was a problem
+from sympy.matrices import Matrix
 
 import aoc.util
 
@@ -38,13 +38,12 @@ def intersect_location_xy(this, other):
 
 
 def cross_matrix(input):
-    return np.array(
+    return Matrix(
         [
             [0, -input[2], input[1]],
             [input[2], 0, -input[0]],
             [-input[1], input[0], 0],
         ],
-        dtype=np.float64
     )
 
 
@@ -72,44 +71,45 @@ class Solver(aoc.util.Solver):
         return count
 
     def part_two(self) -> int:
-        h0_p = np.array(self.hail[0][0], dtype=np.float64)
-        h0_v = np.array(self.hail[0][1], dtype=np.float64)
+        h0_p = Matrix(self.hail[0][0])
+        h0_v = Matrix(self.hail[0][1])
 
-        h1_p = np.array(self.hail[1][0], dtype=np.float64)
-        h1_v = np.array(self.hail[1][1], dtype=np.float64)
+        h1_p = Matrix(self.hail[1][0])
+        h1_v = Matrix(self.hail[1][1])
 
-        h2_p = np.array(self.hail[2][0], dtype=np.float64)
-        h2_v = np.array(self.hail[2][1], dtype=np.float64)
+        h2_p = Matrix(self.hail[2][0])
+        h2_v = Matrix(self.hail[2][1])
 
-        top = -np.cross(h0_p, h0_v) + np.cross(h1_p, h1_v)
-        bot = -np.cross(h0_p, h0_v) + np.cross(h2_p, h2_v)
+        top = -h0_p.cross(h0_v) + h1_p.cross(h1_v)
+        bot = -h0_p.cross(h0_v) + h2_p.cross(h2_v)
 
-        rhs = np.array([
+        rhs = Matrix([
             [top[0]],
             [top[1]],
             [top[2]],
             [bot[0]],
             [bot[1]],
             [bot[2]],
-        ], dtype=np.float64)
+        ])
 
         ul = cross_matrix(h0_v) - cross_matrix(h1_v)
         ll = cross_matrix(h0_v) - cross_matrix(h2_v)
         ur = -cross_matrix(h0_p) + cross_matrix(h1_p)
         lr = -cross_matrix(h0_p) + cross_matrix(h2_p)
 
-        mat = np.array([
+        mat = Matrix([
             [ul[0, 0], ul[0, 1], ul[0, 2], ur[0, 0], ur[0, 1], ur[0, 2]],
             [ul[1, 0], ul[1, 1], ul[1, 2], ur[1, 0], ur[1, 1], ur[1, 2]],
             [ul[2, 0], ul[2, 1], ul[2, 2], ur[2, 0], ur[2, 1], ur[2, 2]],
             [ll[0, 0], ll[0, 1], ll[0, 2], lr[0, 0], lr[0, 1], lr[0, 2]],
             [ll[1, 0], ll[1, 1], ll[1, 2], lr[1, 0], lr[1, 1], lr[1, 2]],
             [ll[2, 0], ll[2, 1], ll[2, 2], lr[2, 0], lr[2, 1], lr[2, 2]],
-        ], dtype=np.float64)
+        ])
 
         # this apparently doesn't have the right precision
         # inv = np.linalg.inv(mat)
         # res = inv @ rhs
-        res = np.linalg.solve(mat, rhs)
+        # res = np.linalg.solve(mat, rhs)
+        res = mat.LUsolve(rhs)
 
         return round(res[0, 0]) + round(res[1, 0]) + round(res[2, 0])